8de8bb2c238b5daecae295db5bf9e145eeef92bb
sandond77 <sandu@ucdavis.edu>
2018-01-28 22:17:58 -0800

    function for showing items for sale complete

---
+++ bamazonCustomer.js
@@ -0,0 +1,51 @@
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+var Table = require('cli-table2')
+
+var connection = mysql.createConnection({
+	host: "localhost",
+	port: 3306,
+	user: "root",
+	password: "falcon415",
+	database: "bamazon_DB"
+});
+
+connection.connect(function(err){
+	if (err) throw err;
+})
+
+
+function availableItems(){
+	var read = connection.query("SELECT id, product_name, price FROM products", function(err,results){
+		if (err) throw err;
+		console.log("\n\nItems For Sale");
+		console.log("----------------------------------");
+
+		var table = new Table({
+			head: ['id', 'Name', 'Price'],
+		  	chars: { 'top': ' ' , 'top-mid': ' ' , 'top-left': ' ' , 'top-right': ' '
+		         , 'bottom': ' ' , 'bottom-mid': ' ' , 'bottom-left': ' ' , 'bottom-right': ' '
+		         , 'left': ' ' , 'left-mid': ' ' , 'mid': ' ' , 'mid-mid': ' '
+		         , 'right': ' ' , 'right-mid': ' ' , 'middle': ' ' },
+		  	style: { 'padding-left': 0, 'padding-right': 0 }
+		});
+
+		for (var i = 0; i < results.length; i++) {
+			table.push(
+		   		[results[i].id, results[i].product_name, "$" + results[i].price]
+			);
+		}
+		console.log(table.toString());
+	});
+}
+
+availableItems();
+
+
+// inquirer.prompt([
+// 	{
+// 		type: list,
+		
+// 	}
+// ])
+// 	
--- bamazon_DB.sql
+++ bamazon_DB.sql
@@ -4,7 +4,7 @@ CREATE DATABASE bamazon_DB;
 
 USE bamazon_DB;
 
-CREATE TABLE product (
+CREATE TABLE products (
 id INT NOT NULL auto_increment,
 product_name VARCHAR(30) NOT NULL,
 department_name VARCHAR(30) NOT NULL,
@@ -13,35 +13,35 @@ stock_quantity INT NOT NULL,
 PRIMARY KEY(id)
 );
 
-INSERT INTO product (product_name, department_name, price, stock_quantity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("Apple","Produce", 1.00, 450);
 
-INSERT INTO product (product_name, department_name, price, stock_quantity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("Banana","Produce", 0.29, 650);
 
-INSERT INTO product (product_name, department_name, price, stock_quantity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("Pear","Produce", 0.59, 750);
 
-INSERT INTO product (product_name, department_name, price, stock_quantity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("iTouch","Electronics", 299.00, 100);
 
-INSERT INTO product (product_name, department_name, price, stock_quantity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("iPod","Electronics", 199.00, 150);
 
-INSERT INTO product (product_name, department_name, price, stock_quantity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("iPhone","Electronics", 899.00, 3000);
 
-INSERT INTO product (product_name, department_name, price, stock_quantity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("Television","Electronics", 599.99, 200);
 
-INSERT INTO product (product_name, department_name, price, stock_quantity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("Playstation","Electronics", 249.00, 500);
 
-INSERT INTO product (product_name, department_name, price, stock_quantity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("Xbox","Electronics", 299.00, 250);
 
-INSERT INTO product (product_name, department_name, price, stock_quantity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("Watermelon","Product", 2.99, 50);
 
 
-SELECT * FROM product;
+SELECT * FROM products;
